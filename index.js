d723c7aa968b68c7b9257a8587fcb159908eb6ed
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-07-10 12:59:42 +0530

    cli create complete

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
 'use strict'
 
-module.exports = async () => {
-  console.log("create-ui-library");
-}
-module.exports().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+require('./lib/cli')
---
+++ template/default/README.md
@@ -0,0 +1,20 @@
+# {{name}}
+
+> {{description}}
+
+
+## Install
+
+```bash
+npm install --save {{name}}
+```
+
+## Usage
+
+```jsx
+
+```
+
+## License
+
+{{license}} © [{{author}}](https://github.com/{{author}})
